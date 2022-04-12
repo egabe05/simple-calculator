e73401ec8c5263d1a51a9aa1e8a081039dc2d49c
Eric Gaberik <eric.gaberik@Erics-MacBook-Pro.local>
2022-04-12 17:20:47 +0200

    adding todos for future work

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,7 @@ def test_add_three_numbers():
 
     assert result == 20
 
+
 def test_add_many_numbers():
     numbers = range(100)
     calculator = SimpleCalculator()
@@ -26,9 +27,20 @@ def test_add_many_numbers():
 
     assert result == 4950
 
+
+# is this test enough to verify substract works??
+# answer: like I mentioned to only write enough code that the test passes they also recommend in the blog to not be
+# dumb and only implment what the assertion asks for (example: here I returend 2 in the method instead of actually
+# doing the subtraction)  IMO a good balance to this is to parameterize the test to verify everything works with 2 
+# different scenarios
+# TODO: parameterize me to also subtract 9, 3
 def test_subtract_two_numbers():
     calculator = SimpleCalculator()
 
     result = calculator.subtract(5, 3)
 
     assert result == 2
+
+# TODO: requirement: multiply two numbers
+
+# TODO: requirement: multiply many numbers
